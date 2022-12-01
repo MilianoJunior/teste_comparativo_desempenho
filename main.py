a5216694f6002b1a73d19f51f45b8334d4b41e81
Miliano Fernandes de Oliveira Junior <jrmfilho37@gmail.com>
2022-12-01 10:06:34 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 08:33:40 2022
+
+@author: jrmfi
+"""
+
+''' Resultado dos testes '''
+
+Legion5i = {
+            'configuracoes':{
+                                'placa_de_rede': 'Intel(R) Wi-Fi 6 AX201 160MHz',
+                                'processador': 'Intel(R) Core(TM) i7-10750H CPU @2.60GHz 2.59 GHz',
+                                'nucleos':6,
+                                'memoria_ram': '16,0GB DDR4 até 2933 MHz',
+                                'memoria': '512GB SSD M.2 PCIe NVMe',
+                                'gpu_0': 'Intel(R) UHD Graphics',
+                                'gpu_1': 'Placa dedicada NVIDIA® GeForce® RTX 2060 6GB GDDR6',
+                            },
+            'teste_placa_de_rede': {
+                                    'descricao': 'teste velocidade internet no navegador',
+                                    'resultado':{'download': '74.4 Mbs' ,'upload':'31.2 Mbs'},
+                }
+    }
+
+
+DellG15 = {
+            'configuracoes':{
+                                'placa_de_rede': 'Intel(R)  Wi-Fi 6 AX1650 160MHz',
+                                'processador': '12ª geração Intel® Core™ i7-12700H 4.7GHz',
+                                'nucleos':14,
+                                'memoria_ram': '16,0GB DDR5 4800 MHz',
+                                'memoria': 'SSD de 512GB PCIe NVMe M.2',
+                                'gpu_0': 'Gráficos Intel® Iris® Xe elegíveis',
+                                'gpu_1': 'NVIDIA® GeForce® RTX™ 3060, 6GB GDDR6',
+                            },
+            'teste_placa_de_rede': {
+                                    'descricao': 'teste velocidade internet no navegador',
+                                    'resultado':{'download': '47.9 Mbs' ,'upload':'32.5 Mbs'},
+                }
+    }
+
